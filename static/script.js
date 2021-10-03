22f689890e85c92bb5cfcfa493ba6fc11d1d7c64
DominiqueLe <88290570+DominiqueLe@users.noreply.github.com>
2021-10-03 15:30:29 -0500

    Add files via upload

---
+++ static/script.js
@@ -0,0 +1,111 @@
+var options = {
+  chart: {
+    height: 350,
+    type: "line",
+    stacked: false
+  },
+  dataLabels: {
+    enabled: false
+  },
+  colors: ["#FF1654", "#247BA0"],
+  series: [
+    {
+      name: "Actual",
+      data: [1.4, 2, 2.5, 1.5, 2.5, 2.8, 3.8, 4.6]
+    },
+    {
+      name: "Budget",
+      data: [20, 29, 37, 36, 44, 45, 50, 58]
+    }
+  ],
+  stroke: {
+    width: [4, 4]
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: "20%"
+    }
+  },
+  xaxis: {
+    categories: ["11-20", "11-21", "11-22","11-23","11-24","11-25","11-26","11-27"  ]
+  },
+  yaxis: [
+    {
+      axisTicks: {
+        show: true
+      },
+      axisBorder: {
+        show: true,
+        color: "#FF1654"
+      },
+      labels: {
+        style: {
+          colors: "#FF1654"
+        }
+      },
+      title: {
+        text: "Actual",
+        style: {
+          color: "#FF1654"
+        }
+      }
+    },
+    {
+      opposite: true,
+      axisTicks: {
+        show: true
+      },
+      axisBorder: {
+        show: true,
+        color: "#247BA0"
+      },
+      labels: {
+        style: {
+          colors: "#247BA0"
+        }
+      },
+      title: {
+        text: "Budget",
+        style: {
+          color: "#247BA0"
+        }
+      }
+    }
+  ],
+  tooltip: {
+    shared: false,
+    intersect: true,
+    x: {
+      show: false
+    }
+  },
+  legend: {
+    horizontalAlign: "left",
+    offsetX: 40
+  }
+};
+
+var chart = new ApexCharts(document.querySelector("#apex1"), options);
+
+chart.render();
+
+
+// Sidebar Toggle Codes;
+
+var sidebarOpen = false;
+var sidebar = document.getElementById("sidebar");
+var sidebarCloseIcon = document.getElementById("sidebarIcon");
+
+function toggleSidebar() {
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar_responsive");
+    sidebarOpen = true;
+  }
+}
+
+function closeSidebar() {
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar_responsive");
+    sidebarOpen = false;
+  }
+}
